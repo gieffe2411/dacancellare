a8d87b8df47cdbf92f51f7590a9a456ac9b672b1
gif <nitritoantico@gmail.com>
2025-11-14 18:16:24 +0100

    Add random curiosity selection in curiosità.py
    creato uno script da cancellare perchè fa schifo

---
+++ curiosità.py
@@ -0,0 +1,11 @@
+import random
+NessunoSapraLeggereQuestaVariabilePercheECosiTantoLungaCheNessunoPuoLeggerlaQuindiAdessoStaNelSecondoRigoQuindiEPraticamenteIllegibileQuestaVariabileDovraPoiEssereRipetutaSottoXDLol = [
+    "Io sono Gabriele"
+    "Una curosità di me in questo momento è che non ho curiosità.",
+    "Curiosità sul codice (non io): questo è index 2",
+    "Boh leggi le altre",
+    "Curiosità sul codice (più sul codice che io): c'è una bellissima variabile chiamata NessunoSapraLeggereQuestaVariabilePercheECosiTantoLungaCheNessunoPuoLeggerlaQuindiAdessoStaNelSecondoRigoQuindiEPraticamenteIllegibileQuestaVariabileDovraPoiEssereRipetutaSottoXDLol",
+    "Ho 0 cani",
+    "Mi piacciono i videogiochi"
+]
+print(random.choice(NessunoSapraLeggereQuestaVariabilePercheECosiTantoLungaCheNessunoPuoLeggerlaQuindiAdessoStaNelSecondoRigoQuindiEPraticamenteIllegibileQuestaVariabileDovraPoiEssereRipetutaSottoXDLol))
